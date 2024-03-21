e0ce5d20ec9b450ee4b05272d7b22890867b2b61
typoscript <minkyu.kim.contact@gmail.com>
2024-03-21 16:37:18 +0900

    Add UserManager Methods

--- shop/src/shop/UserManager.java
+++ shop/src/shop/UserManager.java
@@ -5,10 +5,37 @@ import java.util.ArrayList;
 public class UserManager {
 	private ArrayList<User> users = new ArrayList<User>();
 	
-	public void setUser(int index, User user) {
-		users.set(index, user);
+	public User getUserByIndex(int index) {
+		return this.users.get(index);
 	}
 	
+	public User getUserByIdAndPassword(String id, String password) {
+		User user = getUserById(id);
+		
+		if (user == null || !user.getPassword().equals(password))
+			return null;
+
+		return user;
+	}
+	
+	private User getUserById(String id) {
+		for (User user : users) {
+			if (user.getId() == id)
+				return user;
+		}
+		
+		return null;
+	}
+	
+	private int getIndexOfUser(User user) {
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).equals(user))
+				return i;
+		}
+
+		return -1;
+	}
+
 	public void addUser(User user) {
 		users.add(user);
 	}
@@ -19,13 +46,21 @@ public class UserManager {
 		if (index >= 0)
 			users.remove(index);
 	}
-	
-	private int getIndexOfUser(User user) {
-		for (int i = 0; i < users.size(); i++) {
-			if (users.get(i).equals(user))
-				return i;
-		}
 
-		return -1;
+	public int genereateUserUID() {
+		int id = (int)(Math.random() * 1000000);
+
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).getUID() == id) {
+				id = (int)(Math.random() * 1000);
+				i = -1;
+			}
+		}
+		
+		return id;
+	}
+	
+	public boolean doesUserIdExist(String id) {
+		return getUserById(id) == null ? false : true;
 	}
 }
