e5a72c8b8b3bc10db94f0bf46856788d97e9d3df
typoscript <minkyu.kim.contact@gmail.com>
2024-03-21 18:13:45 +0900

    Add Constructors, Getters, Setters, toString(), clone() Methods

--- shop/src/shop/Item.java
+++ shop/src/shop/Item.java
@@ -1,23 +1,51 @@
 package shop;
 
 public class Item {
-	private String name;
+	private final int UID;
+	private int quantity;
 	private int price;
-	
-	public Item(String name, int price) {
+	private String name;
+
+	public Item(String name, int price, int quantity) {
+		this.UID = -1;
 		this.name = name;
 		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public Item(int uid, String name, int price, int quantity) {
+		this.UID = uid;
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
 	}
 	
 	public String getName() {
-		return this.name;
+		return name;
+	}
+	
+	public int getUID() {
+		return UID;
 	}
 
 	public int getPrice() {
-		return this.price;
+		return price;
+	}
+
+	public int getQuantity() {
+		return price;
+	}
+	
+	public void setCount(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("상품: %s | 가격: %d원 | 수량: %d개", name, price, quantity);
 	}
 	
 	public Item clone() {
-		return new Item(this.name, this.price);
+		return new Item(UID, name, price, quantity);
 	}
 }
