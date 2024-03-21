f1330f682eb0c7a0cba63d77c12a31ebb5067964
typoscript <minkyu.kim.contact@gmail.com>
2024-03-21 18:17:26 +0900

    Modify save(), load() Methods

--- shop/src/shop/FileManager.java
+++ shop/src/shop/FileManager.java
@@ -1,19 +1,46 @@
 package shop;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class FileManager {
-	private static FileManager instance = new FileManager();
+	private static final String FILE_NAME = "data.txt";
+	private static FileWriter fw;
+	private static FileReader fr;
+	private static BufferedReader br;
 
 	private FileManager() { }
 	
-	public FileManager getInstance() {
-		return this.instance;
-	}
-
-	public static void save() {
+	public static void save(String data) {
+		try {
+			fw.write(data);
+			fw.close();
+		} catch (Exception e) {
+			System.out.println("파일 저장 실패");
+			return;
+		}
 		
+		System.out.println("파일 저장 성공");
 	}
 	
-	public static void load() {
+	public static String load() {
+		String data = "";
+
+		try {
+			fr = new FileReader(FILE_NAME);
+			br = new BufferedReader(fr);
+			
+			while (br.ready())
+				data += br.readLine() + "\n";
+			
+			br.close();
+			fr.close();
+		} catch (Exception e) {
+			System.out.println("파일 로드 실패");
+			return "";
+		}
 		
+		return data;
 	}
 }
