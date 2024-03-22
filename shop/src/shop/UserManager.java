3a3ee1afd63a56a595b35bf3b7422755f0cc8a36
typoscript <minkyu.kim.contact@gmail.com>
2024-03-22 18:08:42 +0900

    Add getIndexOfUser(), getUserById(), getUserByUID(), getUserByIdAndPassword(), getUsers(), addUser(arg1), addUser(arg1, arg2), deleteUser(), updateUser(), doesUserIdExist(), generateUserUID() Methods

--- shop/src/shop/UserManager.java
+++ shop/src/shop/UserManager.java
@@ -11,4 +11,78 @@ public class UserManager {
 	public static UserManager getInstance() {
 		return instance;
 	}
+
+	private int getIndexOfUser(User user) {
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).getUID() == user.getUID())
+				return i;
+		}
+		
+		return -1;
+	}
+	
+	private User getUserById(String id) {
+		for (User user : users) {
+			if (user.getId().equals(id))
+				return user;
+		}
+		
+		return null;
+	}
+
+	public User getUserByUID(int UID) {
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).getUID() == UID)
+				return users.get(i).clone();
+		}
+
+		return null;
+	}
+	
+	public User getUserByIdAndPassword(String id, String password) {
+		User user = getUserById(id);
+		
+		if (user == null || !user.getPassword().equals(password))
+			return null;
+
+		return user.clone();
+	}
+
+	public ArrayList<User> getUsers() {
+		return (ArrayList<User>) users.clone();
+	}
+	
+	public void addUser(User user) {
+		users.add(new User(generateUserUID(), user.getId(), user.getPassword(), user.getCart()));
+	}
+
+	public void addUser(User user, int UID) {
+		users.add(new User(UID, user.getId(), user.getPassword(), user.getCart()));
+	}
+
+	public void deleteUser(User user) {
+		User foundUser = getUserById(user.getId());
+		users.remove(foundUser);
+	}
+	
+	public void updateUser(User user) {
+		users.set(getIndexOfUser(user), user);
+	}
+
+	public boolean doesUserIdExist(String id) {
+		return getUserById(id) == null ? false : true;
+	}
+
+	private int generateUserUID() {
+		int id = (int)(Math.random() * 1000000);
+
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).getUID() == id) {
+				id = (int)(Math.random() * 1000);
+				i = -1;
+			}
+		}
+		
+		return id;
+	}
 }
