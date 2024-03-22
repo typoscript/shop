f2bf75965291e9cf0798e1862b3919ad23886836
typoscript <minkyu.kim.contact@gmail.com>
2024-03-22 17:52:41 +0900

    Add "items" variable, Constructor, getItem(), getItems(), getItemByUID(),getIndexOfItemByUID(), setItem(), size(), add(), delete(), hasItem(), clone(), toString() Methods

--- shop/src/shop/Cart.java
+++ shop/src/shop/Cart.java
@@ -1,5 +1,73 @@
 package shop;
 
-public class Cart {
+import java.util.ArrayList;
 
+public class Cart {
+	ArrayList<Item> items;
+
+	public Cart() {
+		this.items = new ArrayList<Item>();
+	}
+	
+	public Cart(ArrayList<Item> items) {
+		this.items = items;
+	}
+
+	private Item getItemByUID(int UID) {
+		int index = getIndexOfItemByUID(UID);
+		return index >= 0 ? items.get(index) : null;
+	}
+
+	public Item getItem(int index) {
+		return items.get(index).clone();
+	}
+
+	public ArrayList<Item> getItems() {
+		return (ArrayList<Item>) items.clone();
+	}
+
+	public int getIndexOfItemByUID(int UID) {
+		for (int i = 0; i < items.size(); i++) {
+			if (items.get(i).getUID() == UID)
+				return i;
+		}
+
+		return -1;
+	}
+
+	public void setItem(int index, Item item) {
+		items.set(index, item);
+	}
+	
+	public int size() {
+		return items.size();
+	}
+	
+	public void add(Item item) {
+		items.add(item);
+	}
+
+	public void delete(Item item) {
+		Item foundItem = getItemByUID(item.getUID());
+		items.remove(foundItem);
+	}
+
+	public boolean hasItem(Item item) {
+		return getItemByUID(item.getUID()) != null;
+	}
+	
+	@Override
+	public Cart clone() {
+		return new Cart(items);
+	}
+	
+	@Override
+	public String toString() {
+		String data = "";
+		
+		for (Item item : items)
+			data += item + "\n";
+			
+		return data;
+	}
 }
