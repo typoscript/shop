fc8a3b4af18b355038ab4214bcc0f7ac572c2dd3
typoscript <minkyu.kim.contact@gmail.com>
2024-03-22 18:01:07 +0900

    Add "instance", "items" variables, private Constructor, getInstance() Method

--- shop/src/shop/ItemManager.java
+++ shop/src/shop/ItemManager.java
@@ -1,5 +1,15 @@
 package shop;
 
+import java.util.ArrayList;
+
 public class ItemManager {
+	private static ItemManager instance = new ItemManager();
+	private ArrayList<Item> items = new ArrayList<Item>();
+	
+	private ItemManager() { }
+	
+	public static ItemManager getInstance() {
+		return instance;
+	}
 
 }
